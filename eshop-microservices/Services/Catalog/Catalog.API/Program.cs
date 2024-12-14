52f8e2a24e6fc8fc5156df4d3bb463d820482ac2
PhucLa1 <phucminhbeos@gmail.com>
2024-12-14 21:04:46 +0700

    base microservice

---
+++ eshop-microservices/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,55 @@
+﻿using BuildingBlocks.Behaviors;
+using BuildingBlocks.Exceptions.Handler;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models; // Import Swagger namespace
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Thêm Swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "My API",
+        Version = "v1",
+        Description = "API documentation for my application",
+    });
+});
+
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+});
+
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+var app = builder.Build();
+
+// Thêm Swagger middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        options.RoutePrefix = string.Empty; // Swagger sẽ hiển thị ở root URL "/"
+    });
+}
+
+app.MapCarter();
+app.UseExceptionHandler(options =>
+{
+
+});
+
+app.Run();
